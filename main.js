5fe476c89a4a1ec2d544b1b8fc04e4cb4211d570
Artem <artemlira84@gmail.com>
2023-12-04 12:18:34 +0200

    commit

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // import './style.css';
 import loader from './loader';
-import beacon from './v0.0.27_v14.6.4/beacon';
-import bundle from './v0.0.27_v14.6.4/bundle';
+import beacon from './public/v0.0.27_v14.6.4/beacon';
+import bundle from './public/v0.0.27_v14.6.4/bundle';
 
 loader();
 bundle();
